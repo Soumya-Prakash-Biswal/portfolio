42c798753450d3b7c047e1e90a6258e3b6e91308
SOUMYAK2528 <imsoumyaprakashbiswal@gmail.com>
2024-03-01 20:16:29 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed=new Typed('.multiple-text', {
+    strings: ["Frontend Developer", "Backend Developer", "Fullstack Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 2000,
+    loop: true
+});
